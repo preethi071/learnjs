13e59b9188c56b595a0e874b86e1438e90bd7781
Preethi <preethi.071@gmail.com>
2015-10-19 22:29:04 +0530

    ton to join
    end of day3

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -4,18 +4,21 @@ function calculate(action){
 	var one= parseInt(document.getElementById("one").value);
 	var two= parseInt(document.getElementById("two").value);
 	var output = null;
-
+	var onenum =true;
+	var twonum =true;
 	if(isNaN(one)){
 		document.getElementById("invalidinput1").innerHTML="Please enter only numbers in first number";
+		onenum=false;
 	}else{
 		document.getElementById("invalidinput1").innerHTML="";
 	}
 	if(isNaN(two)){
 		document.getElementById("invalidinput2").innerHTML="Please enter only numbers in second number";
+		twonum=false;
 	}else{
 		document.getElementById("invalidinput2").innerHTML="";
 	}
-	if(!(isNaN(one) && isNaN(two))){
+	if((onenum)&&(twonum)){
 		if (1==action) {
 			output = one + two ;
 		} else if (2==action) {
@@ -37,5 +40,22 @@ function isnumeric(value){
 		return false;
 	}else{
 		return true;
+	}
+}
+
+function keydownFunction1(){
+	var one= parseInt(document.getElementById("one").value);
+	if(isNaN(one)){
+		document.getElementById("invalidinput1").innerHTML="Please enter only numbers in first number";
+	}else{
+		document.getElementById("invalidinput1").innerHTML="";
+	}
+} 
+function keydownFunction2(){
+	var two= parseInt(document.getElementById("two").value);
+	if(isNaN(two)){
+		document.getElementById("invalidinput2").innerHTML="Please enter only numbers in second number";
+	}else{
+		document.getElementById("invalidinput2").innerHTML="";
 	}
 }
